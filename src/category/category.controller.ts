d0b28d71af0bfa77a16896ab308a214c892ddf88
Ujitha Migara <ujithamigara@gmail.com>
2025-03-02 12:13:49 +0530

    add guards for product and category controllers

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,34 +1,39 @@
-import { Controller, Get, Post, Body, Param, Delete, Query, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Query, Put, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { AdminGuard } from '../auth/admin.guard';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  //TODO: add role guard
-
+  @UseGuards(AdminGuard)
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query('page') page?: number, @Query('limit') limit?: number, @Query('name') name?: string) {
     return this.categoryService.findAll({ page, limit, name });
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(+id);
   }
 
+  @UseGuards(AdminGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoryService.update(+id, updateCategoryDto);
   }
 
+  @UseGuards(AdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id);
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,12 +3,11 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   UseInterceptors,
   UploadedFiles,
-  Query, Put,
+  Query, Put, UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -16,38 +15,44 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import { AdminGuard } from '../auth/admin.guard';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  //TODO: add role guard
-
+  @UseGuards(AdminGuard)
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   async findAll(@Query('page') page?: number, @Query('limit') limit?: number, @Query('name') name?: string, @Query('categoryId') categoryId?: number, @Query('minPrice') minPrice?: number, @Query('maxPrice') maxPrice?: number) {
     return this.productService.findAll({ page, limit, name, categoryId, minPrice, maxPrice });
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
   }
 
+  @UseGuards(AdminGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
+  @UseGuards(AdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
   }
 
+  @UseGuards(AdminGuard)
   @Post(':id/upload-images')
   @UseInterceptors(
     FilesInterceptor('images', 10, {
